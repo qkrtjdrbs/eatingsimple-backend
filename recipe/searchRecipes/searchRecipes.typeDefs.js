89981a7b7844d28d7efdc9da8e39bef9fb592274
qkrtjdrbs <notlce@naver.com>
2021-07-17 22:57:26 +0900

    Result of searchRecipes might be empty

--- recipe/searchRecipes/searchRecipes.typeDefs.js
+++ recipe/searchRecipes/searchRecipes.typeDefs.js
@@ -2,6 +2,6 @@ import { gql } from "apollo-server-express";
 
 export default gql`
   type Query {
-    searchRecipes(keyword: String!): [Recipe]!
+    searchRecipes(keyword: String!): [Recipe]
   }
 `;
