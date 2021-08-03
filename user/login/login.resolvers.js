68704a601d3af81d409c799a65daaef01681aff4
qkrtjdrbs <notlce@naver.com>
2021-08-04 01:18:23 +0900

    Login Network Error Preparation

--- user/login/login.resolvers.js
+++ user/login/login.resolvers.js
@@ -5,27 +5,34 @@ import jwt from "jsonwebtoken";
 export default {
   Mutation: {
     login: async (_, { username, password }) => {
-      const user = await client.user.findUnique({
-        where: { username },
-      });
-      if (!user) {
+      try {
+        const user = await client.user.findUnique({
+          where: { username },
+        });
+        if (!user) {
+          return {
+            ok: false,
+            error: "존재하지 않는 유저입니다",
+          };
+        }
+        const isValidPw = await bcrypt.compare(password, user.password);
+        if (!isValidPw) {
+          return {
+            ok: false,
+            error: "잘못된 패스워드입니다",
+          };
+        }
+        const token = jwt.sign({ id: user.id }, process.env.SECRET_KEY);
+        return {
+          ok: true,
+          token,
+        };
+      } catch (e) {
         return {
           ok: false,
-          error: "존재하지 않는 유저입니다",
+          error: "로그인에 실패했습니다",
         };
       }
-      const isValidPw = await bcrypt.compare(password, user.password);
-      if (!isValidPw) {
-        return {
-          ok: false,
-          error: "잘못된 패스워드입니다",
-        };
-      }
-      const token = jwt.sign({ id: user.id }, process.env.SECRET_KEY);
-      return {
-        ok: true,
-        token,
-      };
     },
   },
 };
